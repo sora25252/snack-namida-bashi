d5fe3627cb29c46a747f2ebb1616fcb7ce059cdb
sora25252 <kanemotokanata38254@yahoo.co.jp>
2025-05-02 03:20:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def handle_message(event):
         )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))  # ← Renderが割り当てたポート番号
+    app.run(host="0.0.0.0", port=port)
